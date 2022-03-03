d5e86881e1aa3186bae82c835d0612597a92a316
BradlyKim <kjh5864@naver.com>
2022-03-03 14:37:56 +0900

    리스트뷰의 실제 데이터 추출, 텍스트뷰 가져오기

--- app/src/main/java/com/neppplus/jickbangcopy_20220227/adapters/RoomAdapter.kt
+++ app/src/main/java/com/neppplus/jickbangcopy_20220227/adapters/RoomAdapter.kt
@@ -5,8 +5,10 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import android.widget.TextView
 import com.neppplus.jickbangcopy_20220227.R
 import com.neppplus.jickbangcopy_20220227.datas.RoomData
+import org.w3c.dom.Text
 
 //    ListView 사용법 메뉴얼 -> 4번 항목 작업
 class RoomAdapter(
@@ -27,9 +29,18 @@ class RoomAdapter(
 
         val row = tempRow!!
 
+//        여기까지가 (아래 return row 포함) room_list_item 생성한 1목록을 어댑터에 담아두는 코드임.
+
+//    ListView 사용법 메뉴얼 -> 7번 항목 작업
+        val data = mList[position]
+
+        val txtPrice = row.findViewById<TextView>(R.id.txtPrice)
+        val txtAddressFloor = row.findViewById<TextView>(R.id.txtAddressFloor)
+        val txtDescription = row.findViewById<TextView>(R.id.txtDescription)
+
+
         return row
 
-//        여기까지가 room_list_item 생성한 1목록을 어댑터에 담아두는 코드임.
 
     }
 
