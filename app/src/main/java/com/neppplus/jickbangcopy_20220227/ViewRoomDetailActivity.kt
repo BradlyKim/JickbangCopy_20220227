f9b5a79b6ff3568a2dc8353e290662521bb4014c
BradlyKim <kjh5864@naver.com>
2022-03-03 16:19:57 +0900

    받아온 데이터를 이용, 실제 UI 에 반영

--- app/src/main/java/com/neppplus/jickbangcopy_20220227/ViewRoomDetailActivity.kt
+++ app/src/main/java/com/neppplus/jickbangcopy_20220227/ViewRoomDetailActivity.kt
@@ -3,6 +3,7 @@ package com.neppplus.jickbangcopy_20220227
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.neppplus.jickbangcopy_20220227.datas.RoomData
+import kotlinx.android.synthetic.main.activity_view_room_detail.*
 
 class ViewRoomDetailActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,7 +13,11 @@ class ViewRoomDetailActivity : AppCompatActivity() {
 //        RoomData, MainActivity 에서 만든 Serializable -> RoomData 로 가져다쓰는 명령어
         val roomData = intent.getSerializableExtra("room") as RoomData
 
-
+//        activity_view_room_detail 에서 자료를 가져와야 함.
+        txtPrice.text = roomData.getFormattedPrice()
+        txtDescription.text = roomData.description
+        txtAddress.text = roomData.address
+        txtFloor.text = roomData.getFormattedFloor()
 
     }
 }
