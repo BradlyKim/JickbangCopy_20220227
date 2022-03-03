f67f7658bce8a2840e2f0d594f79b75cced9d6e1
BradlyKim <kjh5864@naver.com>
2022-03-03 13:29:57 +0900

    어댑터 클래스의 getView 함수 수정  - 그려둔 room_list_item.xml 을 inflate해서, 객체로 만들어서 리턴

--- app/src/main/java/com/neppplus/jickbangcopy_20220227/adapters/RoomAdapter.kt
+++ app/src/main/java/com/neppplus/jickbangcopy_20220227/adapters/RoomAdapter.kt
@@ -1,12 +1,35 @@
 package com.neppplus.jickbangcopy_20220227.adapters
 
 import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import com.neppplus.jickbangcopy_20220227.R
 import com.neppplus.jickbangcopy_20220227.datas.RoomData
 
 class RoomAdapter(
     val mContext : Context,
     val resdId : Int,
     val mList : ArrayList<RoomData>,
-) : ArrayAdapter<RoomData>(mContext, resdId, mList ) {
+) : ArrayAdapter<RoomData>(mContext, resdId, mList ) {   //  ctrl + P => 지원 생성자 목록조회
+
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+
+        var tempRow = convertView
+        if (tempRow == null) {
+
+            tempRow = LayoutInflater.from(mContext).inflate(R.layout.room_list_item, null)
+
+        }
+
+        val row = tempRow!!
+
+        return row
+
+//        여기까지가 room_list_item 생성한 1목록을 어댑터에 담아두는 코드임.
+
+    }
+
 }
